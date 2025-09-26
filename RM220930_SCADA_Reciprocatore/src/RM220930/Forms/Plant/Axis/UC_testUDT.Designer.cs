f4953a68d7b5d640e31aa7e4d0b3c4c440df0fc9
Francesco Paparatti <francesco@rmsoftwaresolutions.ch>
2025-09-26 08:34:48 +0200

    Add: Aggiuto tasto BOOL per verifica funzionamento comunicazione in UC_testUDT

--- RM220930_SCADA_Reciprocatore/src/RM220930/Forms/Plant/Axis/UC_testUDT.Designer.cs
+++ RM220930_SCADA_Reciprocatore/src/RM220930/Forms/Plant/Axis/UC_testUDT.Designer.cs
@@ -28,36 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Cmd_On_Axe = new System.Windows.Forms.Button();
-            this.btn_Cmd_En_Axe = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe1 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe8 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe7 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe6 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe5 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe4 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe3 = new System.Windows.Forms.Button();
+            this.btn_Cmd_On_Axe2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_Cmd_On_Axe
+            // btn_Cmd_On_Axe1
             // 
-            this.btn_Cmd_On_Axe.Location = new System.Drawing.Point(21, 13);
-            this.btn_Cmd_On_Axe.Name = "btn_Cmd_On_Axe";
-            this.btn_Cmd_On_Axe.Size = new System.Drawing.Size(85, 23);
-            this.btn_Cmd_On_Axe.TabIndex = 0;
-            this.btn_Cmd_On_Axe.Text = "Cmd_On_Axe";
-            this.btn_Cmd_On_Axe.UseVisualStyleBackColor = true;
-            this.btn_Cmd_On_Axe.Click += new System.EventHandler(this.btn_Cmd_On_Axe_Click);
+            this.btn_Cmd_On_Axe1.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe1.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe1.Location = new System.Drawing.Point(17, 16);
+            this.btn_Cmd_On_Axe1.Name = "btn_Cmd_On_Axe1";
+            this.btn_Cmd_On_Axe1.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe1.TabIndex = 353;
+            this.btn_Cmd_On_Axe1.Text = "FALSE";
+            this.btn_Cmd_On_Axe1.UseVisualStyleBackColor = false;
+            this.btn_Cmd_On_Axe1.Click += new System.EventHandler(this.ClickEvent_modifyCmd_On_axe);
             // 
-            // btn_Cmd_En_Axe
+            // btn_Cmd_On_Axe8
             // 
-            this.btn_Cmd_En_Axe.Location = new System.Drawing.Point(21, 42);
-            this.btn_Cmd_En_Axe.Name = "btn_Cmd_En_Axe";
-            this.btn_Cmd_En_Axe.Size = new System.Drawing.Size(85, 23);
-            this.btn_Cmd_En_Axe.TabIndex = 1;
-            this.btn_Cmd_En_Axe.Text = "Cmd_En_Axe";
-            this.btn_Cmd_En_Axe.UseVisualStyleBackColor = true;
-            this.btn_Cmd_En_Axe.Click += new System.EventHandler(this.btn_Cmd_En_Axe_Click);
+            this.btn_Cmd_On_Axe8.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe8.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe8.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe8.Location = new System.Drawing.Point(17, 408);
+            this.btn_Cmd_On_Axe8.Name = "btn_Cmd_On_Axe8";
+            this.btn_Cmd_On_Axe8.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe8.TabIndex = 354;
+            this.btn_Cmd_On_Axe8.Text = "FALSE";
+            this.btn_Cmd_On_Axe8.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe7
+            // 
+            this.btn_Cmd_On_Axe7.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe7.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe7.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe7.Location = new System.Drawing.Point(17, 352);
+            this.btn_Cmd_On_Axe7.Name = "btn_Cmd_On_Axe7";
+            this.btn_Cmd_On_Axe7.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe7.TabIndex = 355;
+            this.btn_Cmd_On_Axe7.Text = "FALSE";
+            this.btn_Cmd_On_Axe7.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe6
+            // 
+            this.btn_Cmd_On_Axe6.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe6.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe6.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe6.Location = new System.Drawing.Point(17, 296);
+            this.btn_Cmd_On_Axe6.Name = "btn_Cmd_On_Axe6";
+            this.btn_Cmd_On_Axe6.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe6.TabIndex = 356;
+            this.btn_Cmd_On_Axe6.Text = "FALSE";
+            this.btn_Cmd_On_Axe6.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe5
+            // 
+            this.btn_Cmd_On_Axe5.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe5.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe5.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe5.Location = new System.Drawing.Point(17, 240);
+            this.btn_Cmd_On_Axe5.Name = "btn_Cmd_On_Axe5";
+            this.btn_Cmd_On_Axe5.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe5.TabIndex = 357;
+            this.btn_Cmd_On_Axe5.Text = "FALSE";
+            this.btn_Cmd_On_Axe5.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe4
+            // 
+            this.btn_Cmd_On_Axe4.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe4.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe4.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe4.Location = new System.Drawing.Point(17, 184);
+            this.btn_Cmd_On_Axe4.Name = "btn_Cmd_On_Axe4";
+            this.btn_Cmd_On_Axe4.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe4.TabIndex = 358;
+            this.btn_Cmd_On_Axe4.Text = "FALSE";
+            this.btn_Cmd_On_Axe4.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe3
+            // 
+            this.btn_Cmd_On_Axe3.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe3.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe3.Location = new System.Drawing.Point(17, 128);
+            this.btn_Cmd_On_Axe3.Name = "btn_Cmd_On_Axe3";
+            this.btn_Cmd_On_Axe3.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe3.TabIndex = 359;
+            this.btn_Cmd_On_Axe3.Text = "FALSE";
+            this.btn_Cmd_On_Axe3.UseVisualStyleBackColor = false;
+            // 
+            // btn_Cmd_On_Axe2
+            // 
+            this.btn_Cmd_On_Axe2.BackColor = System.Drawing.Color.Red;
+            this.btn_Cmd_On_Axe2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Cmd_On_Axe2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btn_Cmd_On_Axe2.ForeColor = System.Drawing.Color.Black;
+            this.btn_Cmd_On_Axe2.Location = new System.Drawing.Point(17, 72);
+            this.btn_Cmd_On_Axe2.Name = "btn_Cmd_On_Axe2";
+            this.btn_Cmd_On_Axe2.Size = new System.Drawing.Size(80, 50);
+            this.btn_Cmd_On_Axe2.TabIndex = 360;
+            this.btn_Cmd_On_Axe2.Text = "FALSE";
+            this.btn_Cmd_On_Axe2.UseVisualStyleBackColor = false;
             // 
             // UC_testUDT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btn_Cmd_En_Axe);
-            this.Controls.Add(this.btn_Cmd_On_Axe);
+            this.Controls.Add(this.btn_Cmd_On_Axe2);
+            this.Controls.Add(this.btn_Cmd_On_Axe3);
+            this.Controls.Add(this.btn_Cmd_On_Axe4);
+            this.Controls.Add(this.btn_Cmd_On_Axe5);
+            this.Controls.Add(this.btn_Cmd_On_Axe6);
+            this.Controls.Add(this.btn_Cmd_On_Axe7);
+            this.Controls.Add(this.btn_Cmd_On_Axe8);
+            this.Controls.Add(this.btn_Cmd_On_Axe1);
             this.Name = "UC_testUDT";
             this.Size = new System.Drawing.Size(1024, 557);
             this.ResumeLayout(false);
@@ -65,8 +162,13 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_Cmd_On_Axe;
-        private System.Windows.Forms.Button btn_Cmd_En_Axe;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe1;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe8;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe7;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe6;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe5;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe4;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe3;
+        private System.Windows.Forms.Button btn_Cmd_On_Axe2;
     }
 }
--- RM220930_SCADA_Reciprocatore/src/RM220930/Forms/Plant/Axis/UC_testUDT.cs
+++ RM220930_SCADA_Reciprocatore/src/RM220930/Forms/Plant/Axis/UC_testUDT.cs
@@ -1,4 +1,5 @@
-﻿using RMLib.PLC;
+﻿using RM.Properties;
+using RMLib.PLC;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,14 +14,76 @@ namespace RM.src.RM220930.Forms.Plant.Axis
 {
     public partial class UC_testUDT : UserControl
     {
+        bool Cmd_On_Axe = false;
+
         public UC_testUDT()
         {
             InitializeComponent();
+            InitVar();
+            // Collegamento evento ValueChanged del dizionario al metodo HandleDictionaryChange
+            PLCConfig.appVariables.ValueChanged += RefreshVariables;
         }
 
-        private void btn_Cmd_On_Axe_Click(object sender, EventArgs e)
+        private void InitVar()
         {
-            RefresherTask.AddUpdate("PLC1_" + "axe" + UC_axis.axeOffset.ToString() + "_" + "Cmd_On_Axe", true, "BOOL");
+            Cmd_On_Axe = Convert.ToBoolean(PLCConfig.appVariables.getValue
+                (("PLC1_" + "axe" + UC_axis.axeOffset.ToString() + "_" + "Cmd_On_Axe")));
+
+            if (Cmd_On_Axe)
+            {
+                btn_Cmd_On_Axe1.BackColor = Color.Green;
+                btn_Cmd_On_Axe1.Text = "TRUE";
+            }
+            else
+            {
+                btn_Cmd_On_Axe1.BackColor = Color.Red;
+                btn_Cmd_On_Axe1.Text = "FALSE";
+            }
+        }
+
+        /// <summary>
+        /// Metodo richiamato dall'evento ValueChanged del dizionario delle variabili PLC
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void RefreshVariables(object sender, DictionaryChangedEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<object, DictionaryChangedEventArgs>(RefreshVariables), sender, e);
+                return;
+            }
+
+            string key = e.Key;
+            string expectedKey = $"PLC1_axe{UC_axis.axeOffset}_Cmd_On_Axe";
+
+            if (key == expectedKey)
+            {
+                if (e.NewValue.ToString() == "True")
+                {
+                    btn_Cmd_On_Axe1.BackColor = Color.Green;
+                    btn_Cmd_On_Axe1.Text = "TRUE";
+                    Cmd_On_Axe = true;
+                }
+                else
+                {
+                    btn_Cmd_On_Axe1.BackColor = Color.Red;
+                    btn_Cmd_On_Axe1.Text = "FALSE";
+                    Cmd_On_Axe = false;
+                }
+            }
+
+
+
+        }
+    
+
+        private void ClickEvent_modifyCmd_On_axe(object sender, EventArgs e)
+        {
+            if (!Cmd_On_Axe)
+                RefresherTask.AddUpdate("PLC1_" + "axe" + UC_axis.axeOffset.ToString() + "_" + "Cmd_On_Axe", true, "BOOL");
+            else
+                RefresherTask.AddUpdate("PLC1_" + "axe" + UC_axis.axeOffset.ToString() + "_" + "Cmd_On_Axe", false, "BOOL"); 
         }
 
         private void btn_Cmd_En_Axe_Click(object sender, EventArgs e)
